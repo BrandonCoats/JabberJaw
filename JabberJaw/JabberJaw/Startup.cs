a09dd7e8e800a300d1a3e2b5115ee22d9c0c6162
BrandonCoats <bgcoats1313@gmail.com>
2017-10-04 09:55:55 -0600

    Final fix for mvc setup
    Found a nice explanation of mvc too :+1:

---
+++ JabberJaw/JabberJaw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JabberJaw.Startup))]
+namespace JabberJaw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
