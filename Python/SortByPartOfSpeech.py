e00d9a93ce938ad8677c2f4b756991ec55781fa0
BrandonCoats <bgcoats1313@gmail.com>
2017-10-18 08:09:23 -0600

    tons of practice with nltk

---
+++ Python/SortByPartOfSpeech.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+train_text = state_union.raw("2005-GWBush.txt");
+sample_text = state_union.raw("2006-GWBush.txt");
+
+# the above are native files in the library for training
+# below is the way to train the tokenizer
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+
+def process_content():
+    try:
+        # this line defines the
+        for i in tokenized[:5]:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            print(tagged)
+
+    except Exception as e:
+        print(str(e))
+
+
+process_content()
