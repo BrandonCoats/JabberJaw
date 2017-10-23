4b45d54852b1411a44a0ae108c4404d490a88b27
BrandonCoats <bgcoats1313@gmail.com>
2017-10-23 09:06:22 -0600

    Tons of python work forgot to push the stuff

---
+++ Python/LearningPython/TipsAndTricks.py
@@ -0,0 +1,4 @@
+# virtual enviroments
+# pip install vitualenv
+# vitrualenv <name>
+# vitualenv --python=2.7 version data path
---
+++ Python/LearningPython/webapp/WebAppLearning.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, render_template, redirect, url_for, request
+
+from LearningPython.classesAndOOP import *
+
+students = []
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def students_page():
+    if request.method == "POST":
+        new_student_id = request.form.get("student-id", "")
+        new_student_name = request.form.get("name", "")
+        new_student_last_name = request.form.get("last-name", "")
+
+        new_student = Student(name=new_student_name, student_id=new_student_id)
+        students.append(new_student)
+
+        return redirect(url_for("students_page"))
+
+    return render_template("index.html", students=students)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+"""
+great example of tons of stuff , install, and web junk
+"""
