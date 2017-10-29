61383680309d77c01d46d601e6446ade421290a4
BrandonCoats <bgcoats1313@gmail.com>
2017-10-29 10:55:20 -0600

    Changed file path trying to fix the command args'

--- Python/Chunking.py
+++ Python/Chunking.py
@@ -4,29 +4,29 @@ from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
 import sys
 print int(sys.argv[1]) + int(sys.argv[2])
-train_text = state_union.raw("2005-GWBush.txt")
-sample_text = state_union.raw("2006-GWBush.txt")
-
-custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
-
-tokenized = custom_sent_tokenizer.tokenize(sample_text)
-
-
-def process_content():
-    try:
-        for i in tokenized:
-            words = nltk.word_tokenize(i)
-            tagged = nltk.pos_tag(words)
-            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
-            chunkParser = nltk.RegexpParser(chunkGram)
-            chunked = chunkParser.parse(tagged)
-            chunked.draw()
-
-    except Exception as e:
-        print(str(e))
-
-
-process_content()
+# train_text = state_union.raw("2005-GWBush.txt")
+# sample_text = state_union.raw("2006-GWBush.txt")
+#
+# custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+#
+# tokenized = custom_sent_tokenizer.tokenize(sample_text)
+#
+#
+# def process_content():
+#     try:
+#         for i in tokenized:
+#             words = nltk.word_tokenize(i)
+#             tagged = nltk.pos_tag(words)
+#             chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
+#             chunkParser = nltk.RegexpParser(chunkGram)
+#             chunked = chunkParser.parse(tagged)
+#             chunked.draw()
+#
+#     except Exception as e:
+#         print(str(e))
+#
+#
+# process_content()
 
 # this retrievs the chucks and the non chunks as an array and rolls through them
 # for subtree in chunked.subtrees():
--- TestingLinkage/TestingLinkage/Program.cs
+++ TestingLinkage/TestingLinkage/Program.cs
@@ -13,14 +13,15 @@ namespace TestingLinkage
         static void Main(string[] args)
         {
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = "C:\\Python27\\python.exe";
-            Console.Write(args.Length);
+            start.FileName = "C:\\dev\\Python\\python2\\python.exe";
+            //start.FileName = "C:\\dev\\Capstone\\JabberJaw(Working Tiitle)\\JabberJaw\\Python\\Chunking.py";
+            //Console.Write(args.Length);
             // arg[0] = Path to your python script (example : "C:\\add_them.py")
             // arg[1] = first arguement taken from  C#'s main method's args variable (here i'm passing a number : 5)
             // arg[2] = second arguement taken from  C#'s main method's args variable ( here i'm passing a number : 6)
             // pass these to your Arguements property of your ProcessStartInfo instance
 
-            start.Arguments = string.Format("{0} {1} {2}", args[0], args[1], args[2]);
+            start.Arguments = string.Format("{0} {1} {2}", "C:\\dev\\Capstone\\JabberJaw(Working Tiitle)\\JabberJaw\\Python\\Chunking.py", 5,6);
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
             using (Process process = Process.Start(start))
